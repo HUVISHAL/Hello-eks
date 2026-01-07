36c3bf655bd46ef451afce492e11617ee52a4dc7
H U VISHAL <hu.vishal23@gmail.com>
2026-01-07 21:31:45 +0530

    Update index.js

--- App/index.js
+++ App/index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Hello World from EKS via GitHub Actions!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
 
